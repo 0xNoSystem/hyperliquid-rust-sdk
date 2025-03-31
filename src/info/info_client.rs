4c01507297935922003fa43c20bf119f8d4b2e40
0xNoSystem <141743613+0xNoSystem@users.noreply.github.com>
2025-03-31 10:57:42 +0200

    revert info_client.rs

--- src/info/info_client.rs
+++ src/info/info_client.rs
@@ -105,7 +105,7 @@ pub struct InfoClient {
 
 impl InfoClient {
     pub async fn new(client: Option<Client>, base_url: Option<BaseUrl>) -> Result<InfoClient> {
-        Self::new_internal(client, base_url, true).await
+        Self::new_internal(client, base_url, false).await
     }
 
     pub async fn with_reconnect(

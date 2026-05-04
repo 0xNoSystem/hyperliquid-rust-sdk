07c4daa87016697a5b2c9f077dcc40c72f90a932
0xNoSystem <joesawayajsx@gmail.com>
2026-05-04 12:56:44 +0300

    add HIP-3 logic in wallet margin fetching

--- src/info/info_client.rs
+++ src/info/info_client.rs
@@ -35,6 +35,8 @@ pub enum InfoRequest {
     #[serde(rename = "clearinghouseState")]
     UserState {
         user: Address,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        dex: Option<String>,
     },
     #[serde(rename = "batchClearinghouseStates")]
     UserStates {
@@ -52,6 +54,8 @@ pub enum InfoRequest {
     },
     FrontendOpenOrders {
         user: Address,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        dex: Option<String>,
     },
     OrderStatus {
         user: Address,
@@ -223,13 +227,18 @@ impl InfoClient {
     pub async fn frontend_open_orders(
         &self,
         address: Address,
+        dex: Option<String>,
     ) -> Result<Vec<FrontendOpenOrdersResponse>> {
-        let input = InfoRequest::FrontendOpenOrders { user: address };
+        let input = InfoRequest::FrontendOpenOrders { user: address, dex };
         self.send_info_request(input).await
     }
 
-    pub async fn user_state(&self, address: Address) -> Result<UserStateResponse> {
-        let input = InfoRequest::UserState { user: address };
+    pub async fn user_state(
+        &self,
+        address: Address,
+        dex: Option<String>,
+    ) -> Result<UserStateResponse> {
+        let input = InfoRequest::UserState { user: address, dex };
         self.send_info_request(input).await
     }
 
